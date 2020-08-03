610b20886c32198efd82a47970a28a050944965c
saowalak.tb@rmuti.ac.th <b0613384510>
2020-08-04 04:58:11 +0700

    05.3-5-step2

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -53,10 +53,11 @@ class ScoreFragment : Fragment() {
         viewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(ScoreViewModel::class.java)
         binding.scoreViewModel = viewModel
+        // Specify the fragment view as the lifecycle owner of the binding.
+// This is used so that the binding can observe LiveData updates
+        binding.lifecycleOwner = viewLifecycleOwner
         // Add observer for score
-        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
-            binding.scoreText.text = viewModel.score.toString()
-        })
+
         binding.scoreText.text = viewModel.score.toString()
         binding.playAgainButton.setOnClickListener {  viewModel.onPlayAgain()  }
 
